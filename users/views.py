2b4c68275d22ac33ad801b4e456e0c52e84df916
alexeyzakirov <zakirov.alexey@gmail.com>
2024-05-31 22:51:07 +0300

    first commit

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from users.models import Payments
+
+
+class PaymentViewSet(ModelViewSet):
+    queryset = Payments.objects.all()
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ("course", "lesson", "method")
+    ordering_fields = ("date",)
